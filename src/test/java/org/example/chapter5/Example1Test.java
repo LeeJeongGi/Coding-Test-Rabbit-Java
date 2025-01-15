f730fe51ebf4a1f7aa9ca1f9e4d56250a5eed324
LeeJeongGi <apple4rhk@naver.com>
2025-01-16 06:35:06 +0900

    chapter5: 1번 문제 - 정렬하기 매우매우매우매우매우 간단

--- src/test/java/org/example/chapter5/Example1Test.java
+++ src/test/java/org/example/chapter5/Example1Test.java
@@ -1,6 +1,10 @@
 package org.example.chapter5;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,8 +16,22 @@ class Example1Test {
         int[] arr = {1, -5, 2, 4, 3};
         int[] result = example1.solution(arr);
 
-        System.out.println(result.toString());
+        Arrays.stream(result).forEach(System.out::print);
+        System.out.println(Arrays.toString(result));
         assertArrayEquals(result, arr);
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"1,-5,2,4,3", "2,1,1,3,2,5,4", "6,1,7"})
+    void test2(String input) {
+        int[] arr = Arrays.stream(input.split(",")).mapToInt(Integer::parseInt).toArray();
+
+        Arrays.stream(arr).forEach(System.out::print);
+        Example1 example1 = new Example1();
+        int[] result = example1.solution(arr);
+
+        Arrays.stream(result).forEach(System.out::print);
+        assertArrayEquals(result, arr);
+        System.out.println();
+    }
 }
